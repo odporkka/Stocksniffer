d591819e1cc703ed8c88bcafb227dd84d06d98d0
Otto Porkka <ode.porkka@gmail.com>
2017-05-18 22:00:40 +0300

    Test files generated. Removed unused test/ folder

--- spec/models/nasdaq_instrument_spec.rb
+++ spec/models/nasdaq_instrument_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe XetraInstrument, type: :model do
+RSpec.describe NasdaqInstrument, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
